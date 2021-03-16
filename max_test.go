01d27450abe71ebd68889e1d0762b1e20360f2d1
ADarien <aalian.darien@gmail.com>
2021-03-16 22:15:28 +0700

    add testTable and Logf

--- max_test.go
+++ max_test.go
@@ -4,17 +4,38 @@ import "testing"
 
 func TestMax(t *testing.T) {
 	// Arrange
-	numbers := []int{1, 2, 5, 7, -2, 15, 14, -15}
-	expected := 15
+	testTable := []struct {
+		numbers  []int
+		expected int
+	}{
+		{
+			numbers:  []int{1, 2, 5, 7, -2, 15, 14, -15},
+			expected: 15,
+		},
+		{
+			numbers:  []int{0, -2, -10},
+			expected: 0,
+		},
+		{
+			numbers:  []int{},
+			expected: 0,
+		},
+	}
 
 	//Act
-	result := Max(numbers)
+	for _, testCase := range testTable {
+		result := Max(testCase.numbers)
 
-	// Assert
-	if result != expected {
-		t.Errorf("Incorrect result. Expect %d, got %d",
-			expected, result)
+		t.Logf("Calling Max(%v), result %d\n",
+			testCase.numbers, result)
+
+		// Assert
+		if result != testCase.expected {
+			t.Errorf("Incorrect result. Expect %d, got %d",
+				testCase.expected, result)
+		}
 	}
+
 }
 
 func TestMaxIndex(t *testing.T) {
